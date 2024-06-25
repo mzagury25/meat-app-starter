542c70fc3bca919a5bb0a8438aafeccc5fd867ff
zagur <zagury42@gmail.com>
2024-06-25 15:04:37 -0300

    commit10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import {Routes} from '@angular/router'
 
 import {HomeComponent} from './home/home.component'
 
-import {About} from './about/about.component'
+import {AboutComponent} from './about/about.component'
+
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent}
 ]
