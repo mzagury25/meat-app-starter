3e5df99507a8be768ea9b3ff21c8533642d66e7e
zagur <zagury42@gmail.com>
2024-06-25 15:27:10 -0300

    commit-testAlterations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from "@angular/core"
 
 @Component({
-  selector: 'mt-app',
+  selector: 'mt',
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
 
-  content = 'Bem vindo ao Meat App!'
+  //content = 'Bem vindo ao Meat App!'
 
   constructor() { }
 
