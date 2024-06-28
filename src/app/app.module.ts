4ea0388efb3cf04f707094ca2f7da07952b02dde
zagur <zagury42@gmail.com>
2024-06-28 14:51:33 -0300

    commitchanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import {ROUTES} from  './app.routes'
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
-import { ROUTES } from './app.routes';
+import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/src/app/restaurants/restaurant/restaurant.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantsService } from './restaurants/restaurants.service'
 
 
 @NgModule({
@@ -27,7 +28,7 @@ import { RestaurantComponent } from './restaurants/src/app/restaurants/restauran
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
