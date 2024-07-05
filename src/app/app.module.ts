af69ae2621e6c882b77e6a12f529b5af75e0c4c4
zagur <zagury42@gmail.com>
2024-07-05 16:09:07 -0300

    changesCorrectionWorked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/src/app/restaurants/restaurant/restaurant.component';
-
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+                                    
 
 @NgModule({
   declarations: [
