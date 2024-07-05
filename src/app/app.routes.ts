1f4d8955b0c01c122034cca92909b9deebc1e0b1
zagur <zagury42@gmail.com>
2024-07-05 12:35:06 -0300

    addRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ import {HomeComponent} from './home/home.component'
 
 import {AboutComponent} from './about/about.component'
 
+import {RestaurantsComponent} from './restaurants/restaurants.component'
+
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},
     {path: 'about', component: AboutComponent}
 ]
