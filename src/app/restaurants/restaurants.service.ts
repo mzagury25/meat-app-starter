b285c1fb71aee05d6d93563e580f60000f25e4cc
zagur <zagury42@gmail.com>
2024-07-06 18:09:05 -0300

    att447

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Restaurant } from './restaurant/restaurant.model';
-import { MEAT_API } from './app.api';
-
+import { MEAT_API } from './../app.api';
+ 
+import { Injectable } from '@angular/core';
+ 
+import { Observable } from 'rxjs';
+ 
+import { HttpClient } from '@angular/common/http';
+ 
 @Injectable()
-export class RestaurantsService {
-
-  constructor(private http: HttpClient) { }
-
+export class RestaurantServices {
+ 
+  constructor(private http: HttpClient) {}
+ 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`);
   }
