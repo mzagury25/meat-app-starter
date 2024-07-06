779dd9668286867c7f3368df369ecbea67217a04
zagur <zagury42@gmail.com>
2024-07-06 17:21:17 -0300

    novasAtt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service'                                
+import { RestaurantsService } from './restaurants/restaurants.service';
 
 @NgModule({
   declarations: [
@@ -20,17 +19,15 @@ import { RestaurantsService } from './restaurants/restaurants.service'
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent,
-    
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(ROUTES),
-   
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantsService,
+    RestaurantsService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,33 +1,15 @@
-import {Restaurant} from "./restaurant/restaurant.model"
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from './app.api';
 
-
+@Injectable()
 export class RestaurantsService {
 
-    
+  constructor(private http: HttpClient) { }
 
-        rests: Restaurant[] = [
-          {
-            id: "bread-bakery",
-            name: "Bread & Bakery",
-            category: "Bakery",
-            deliveryEstimate: "25m",
-            rating: 4.9,
-            imagePath: "assets/img/restaurants/breadbakery.png"
-          },
-          {
-            id: "burger-house",
-            name: "Burger House",
-            category: "Hamburgers",
-            deliveryEstimate: "100m",
-            rating: 3.5,
-            imagePath: "assets/img/restaurants/burgerhouse.png"
-          }]
-
-
-    constructor(){}
-
-    restaurants(): Restaurant []{
-        return this.rests;
-
-    }
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`);
+  }
 }
